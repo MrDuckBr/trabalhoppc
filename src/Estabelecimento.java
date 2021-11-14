c46fa1ff4fc2a589595e2effa66a08402a759032
MrDuckBr <wwalisson2012@gmail.com>
2021-11-14 15:58:12 -0300

    :rocket: Primeiro commit

--- src/Estabelecimento.java
+++ src/Estabelecimento.java
@@ -1,17 +1,24 @@
+import java.util.ArrayList;
+
 public class Estabelecimento {
 
     int rodadas, clientes, garcons, capacidade;
+    ArrayList<Thread> lstClientes, lstGarcons;
 
     public Estabelecimento(int rodadas, int clientes, int garcons, int capacidade){
         this.clientes = clientes;
         this.garcons = garcons;
         this.rodadas = rodadas;
         this.capacidade = capacidade;
-    }
 
+        lstClientes = new ArrayList<>();
+        lstGarcons = new ArrayList<>();
+    }
+//https://www.javatpoint.com/how-to-create-a-thread-in-java
     public void comecar(){
         System.out.println("Começou o atendimento de rodadas free");
-        
+
+
     }
 
 
